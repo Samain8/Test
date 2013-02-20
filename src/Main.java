29bde91f65de33bc37f3a82473df7946db404d36
Nikita <samain8@gmail.com>
2013-02-21 01:01:42 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nikita
+ * Date: 21.02.13
+ * Time: 0:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
