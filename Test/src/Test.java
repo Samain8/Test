0305a873618786bf36df0d18e2b18e392fc09df4
Nikita <samain8@gmail.com>
2013-02-22 12:34:21 +0400

    init

---
+++ Test/src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nick
+ * Date: 22.02.13
+ * Time: 12:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+
+    }
+}
